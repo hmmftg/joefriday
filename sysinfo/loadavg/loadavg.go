471442a1761bf94e28e81683d94a39a8479a529c
Joel Scoble <joel.scoble@outlook.com>
2016-04-14 16:29:53 -0500

    add syscall based loadavg

---
+++ sysinfo/loadavg/loadavg.go
@@ -0,0 +1,88 @@
+// Copyright 2016 Joel Scoble and The JoeFriday authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package loadavg returns the system;s loadavg information, for 1, 5, and
+// 15 minutes, using syscall.
+package loadavg
+
+import (
+	"syscall"
+	"time"
+
+	joe "github.com/mohae/joefriday"
+)
+
+const LoadsScale = 65536
+
+type Ticker struct {
+	*joe.Ticker
+	Data chan LoadAvg
+}
+
+// NewTicker returns a new Ticker containing a ticker channel, T,
+func NewTicker(d time.Duration) (joe.Tocker, error) {
+	t := Ticker{Ticker: joe.NewTicker(d), Data: make(chan LoadAvg)}
+	go t.Run()
+	return &t, nil
+}
+
+// Stop sends a signal to the done channel, stopping the goroutine.  This
+// also closes all channels that the Ticker holds.
+func (t *Ticker) Stop() {
+	t.Done <- struct{}{}
+}
+
+func (t *Ticker) Run() {
+	defer t.Close()
+	defer close(t.Data)
+	// read until done signal is received
+	for {
+		select {
+		case <-t.Done:
+			return
+		case <-t.Ticker.C:
+			s, err := Get()
+			if err != nil {
+				t.Errs <- err
+				continue
+			}
+			t.Data <- s
+		}
+	}
+}
+
+type LoadAvg struct {
+	One     float64
+	Five    float64
+	Fifteen float64
+}
+
+// Get the load average for the last 1, 5, and 15 minutes.
+func (l *LoadAvg) Get() error {
+	var sysinfo syscall.Sysinfo_t
+	err := syscall.Sysinfo(&sysinfo)
+	if err != nil {
+		return err
+	}
+	l.One = float64(sysinfo.Loads[0]) / LoadsScale
+	l.Five = float64(sysinfo.Loads[1]) / LoadsScale
+	l.Fifteen = float64(sysinfo.Loads[2]) / LoadsScale
+	return nil
+}
+
+// Get returns a LoadAvg populated with the 1, 5, and 15 minute values.
+func Get() (LoadAvg, error) {
+	var l LoadAvg
+	err := l.Get()
+	return l, err
+}
---
+++ sysinfo/loadavg/loadavg_test.go
@@ -0,0 +1,58 @@
+package loadavg
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGet(t *testing.T) {
+	l, err := Get()
+	if err != nil {
+		t.Error(err)
+	}
+	checkLoadAvg("get", l, t)
+}
+
+func TestTicker(t *testing.T) {
+	tkr, err := NewTicker(time.Millisecond)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	tk := tkr.(*Ticker)
+	for i := 0; i < 5; i++ {
+		select {
+		case <-tk.Done:
+			break
+		case v, ok := <-tk.Data:
+			if !ok {
+				break
+			}
+			checkLoadAvg("ticker", v, t)
+		case err := <-tk.Errs:
+			t.Errorf("unexpected error: %s", err)
+		}
+	}
+	tk.Stop()
+}
+
+func checkLoadAvg(n string, l LoadAvg, t *testing.T) {
+	if l.One == 0 {
+		t.Errorf("%s: expected the 1 minute load avg to be non-zero, was 0", n)
+	}
+	if l.Five == 0 {
+		t.Errorf("%s: expected the 5 minute load avg to be non-zero, was 0", n)
+	}
+	if l.Fifteen == 0 {
+		t.Errorf("%s: expected the 15 minute load avg to be non-zero, was 0", n)
+	}
+	t.Logf("%#v\n", l)
+}
+
+func BenchmarkLoadAvg(b *testing.B) {
+	var tmp LoadAvg
+	for i := 0; i < b.N; i++ {
+		_ = tmp.Get()
+	}
+	_ = tmp
+}
