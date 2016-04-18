92ea7c34319a14e9b5019ff6af32f3195f48ed50
Joel Scoble <joel.scoble@outlook.com>
2016-04-17 22:22:06 -0500

    loadavg -> avg

--- sysinfo/load/avg.go
+++ sysinfo/load/avg.go
@@ -11,9 +11,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package loadavg returns the system;s loadavg information, for 1, 5, and
-// 15 minutes, using syscall.
-package loadavg
+// Package avg returns the system;s loadavg information, for 1, 5, and 15
+// minutes, using syscall.
+package avg
 
 import (
 	"syscall"
