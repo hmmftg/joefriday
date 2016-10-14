706cadda0dfeb858e8584410e95bcf811845c268
Joel Scoble <joel.scoble@outlook.com>
2016-10-14 15:15:36 -0500

    add basic meminfo processing: obtains a small subset of data from /proc/meminfo

---
+++ mem/basic/json/meminfo_test.go
@@ -0,0 +1,154 @@
+// Copyright 2016 Joel Scoble and The JoeFriday authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package json
+
+import (
+	"testing"
+	"time"
+
+	"github.com/mohae/joefriday/mem/basic"
+)
+
+func TestGet(t *testing.T) {
+	nf, err := Get()
+	if err != nil {
+		t.Errorf("got %s, want nil", err)
+		return
+	}
+	inf, err := Unmarshal(nf)
+	if err != nil {
+		t.Errorf("got %s, want nil", err)
+		return
+	}
+	checkInfo("get", *inf, t)
+}
+
+func TestTicker(t *testing.T) {
+	tkr, err := NewTicker(time.Millisecond)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	tk := tkr.(*Ticker)
+	for i := 0; i < 5; i++ {
+		select {
+		case <-tk.Done:
+			break
+		case v, ok := <-tk.Data:
+			if !ok {
+				break
+			}
+			inf, err := Unmarshal(v)
+			if err != nil {
+				t.Errorf("got %s, want nil", err)
+				return
+			}
+			checkInfo("ticker", *inf, t)
+		case err := <-tk.Errs:
+			t.Errorf("unexpected error: %s", err)
+		}
+	}
+	tk.Stop()
+	tk.Close()
+}
+
+func checkInfo(n string, i basic.MemInfo, t *testing.T) {
+	if i.Timestamp == 0 {
+		t.Errorf("%s: expected timestamp to be a non-zero value, got 0", n)
+	}
+	if i.Active == 0 {
+		t.Errorf("%s: expected Active to be a non-zero value, got 0", n)
+	}
+	if i.Inactive == 0 {
+		t.Errorf("%s: expected Inactive to be a non-zero value, got 0", n)
+	}
+	if i.Mapped == 0 {
+		t.Errorf("%s: expected Mapped to be a non-zero value, got 0", n)
+	}
+	if i.MemAvailable == 0 {
+		t.Errorf("%s: expected MemAvailable to be a non-zero value, got 0", n)
+	}
+	if i.MemFree == 0 {
+		t.Errorf("%s: expected MemFree to be a non-zero value, got 0", n)
+	}
+	if i.MemTotal == 0 {
+		t.Errorf("%s: expected MemTotal to be a non-zero value, got 0", n)
+	}
+	if i.SwapFree == 0 {
+		t.Errorf("%s: expected SwapFree to be a non-zero value, got 0", n)
+	}
+	if i.SwapTotal == 0 {
+		t.Errorf("%s: expected SwapTotal to be a non-zero value, got 0", n)
+	}
+	t.Logf("%#v\n", i)
+}
+
+func BenchmarkGet(b *testing.B) {
+	var jsn []byte
+	b.StopTimer()
+	p, _ := NewProfiler()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		jsn, _ = p.Get()
+	}
+	_ = jsn
+}
+
+func BenchmarkSerialize(b *testing.B) {
+	var jsn []byte
+	b.StopTimer()
+	p, _ := NewProfiler()
+	v, _ := p.Profiler.Get()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		jsn, _ = p.Serialize(v)
+	}
+	_ = jsn
+}
+
+func BenchmarkMarshal(b *testing.B) {
+	var jsn []byte
+	b.StopTimer()
+	p, _ := NewProfiler()
+	v, _ := p.Profiler.Get()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		jsn, _ = p.Marshal(v)
+	}
+	_ = jsn
+}
+
+var inf *basic.MemInfo
+
+func BenchmarkDeserialize(b *testing.B) {
+	b.StopTimer()
+	p, _ := NewProfiler()
+	tmp, _ := p.Get()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		inf, _ = Deserialize(tmp)
+	}
+	_ = inf
+}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	b.StartTimer()
+	p, _ := NewProfiler()
+	tmp, _ := p.Get()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		inf, _ = Unmarshal(tmp)
+	}
+	_ = inf
+}
---
+++ mem/basic/meminfo_test.go
@@ -0,0 +1,91 @@
+// Copyright 2016 Joel Scoble and The JoeFriday authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package basic
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGet(t *testing.T) {
+	inf, err := Get()
+	if err != nil {
+		t.Errorf("got %s, want nil", err)
+		return
+	}
+	checkInfo("get", *inf, t)
+}
+
+func TestTicker(t *testing.T) {
+	tkr, err := NewTicker(time.Millisecond)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	tk := tkr.(*Ticker)
+	for i := 0; i < 5; i++ {
+		select {
+		case <-tk.Done:
+			break
+		case v, ok := <-tk.Data:
+			if !ok {
+				break
+			}
+			checkInfo("ticker", v, t)
+		case err := <-tk.Errs:
+			t.Errorf("unexpected error: %s", err)
+		}
+	}
+	tk.Stop()
+	tk.Close()
+}
+
+func checkInfo(n string, i MemInfo, t *testing.T) {
+	if i.Timestamp == 0 {
+		t.Errorf("%s: expected timestamp to be a non-zero value, got 0", n)
+	}
+	if i.Active == 0 {
+		t.Errorf("%s: expected Active to be a non-zero value, got 0", n)
+	}
+	if i.Inactive == 0 {
+		t.Errorf("%s: expected Inactive to be a non-zero value, got 0", n)
+	}
+	if i.MemAvailable == 0 {
+		t.Errorf("%s: expected MemAvailable to be a non-zero value, got 0", n)
+	}
+	if i.MemFree == 0 {
+		t.Errorf("%s: expected MemFree to be a non-zero value, got 0", n)
+	}
+	if i.MemTotal == 0 {
+		t.Errorf("%s: expected MemTotal to be a non-zero value, got 0", n)
+	}
+	if i.SwapFree == 0 {
+		t.Errorf("%s: expected SwapFree to be a non-zero value, got 0", n)
+	}
+	if i.SwapTotal == 0 {
+		t.Errorf("%s: expected SwapTotal to be a non-zero value, got 0", n)
+	}
+	t.Logf("%#v\n", i)
+}
+
+func BenchmarkGet(b *testing.B) {
+	var inf *MemInfo
+	b.StopTimer()
+	p, _ := NewProfiler()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		inf, _ = p.Get()
+	}
+	_ = inf
+}
