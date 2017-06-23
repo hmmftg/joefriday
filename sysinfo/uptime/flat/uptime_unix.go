7362f48097f0237498c24e2de541e4825fb4d86f
Joel Scoble <joel.scoble@outlook.com>
2017-06-23 16:47:42 -0500

    add renamed uptime files

---
+++ sysinfo/uptime/flat/uptime_unix.go
@@ -0,0 +1,114 @@
+// Copyright 2016 Joel Scoble and The JoeFriday authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package uptime handles Flatbuffer based processing of the system's uptime
+// using syscall. Instead of returning a Go struct, it returns Flatbuffer
+// serialized bytes. A function to deserialize the Flatbuffer serialized
+// bytes into an uptime.Info struct is provided.  After the first use, the
+// flatbuffer builder is reused.
+//
+// Note: the uptime name is processors and not the final element of the import
+// path (flat). 
+package uptime
+
+import (
+	"sync"
+	"time"
+
+	fb "github.com/google/flatbuffers/go"
+	joe "github.com/mohae/joefriday"
+	up "github.com/mohae/joefriday/sysinfo/uptime"
+	"github.com/mohae/joefriday/sysinfo/uptime/flat/structs"
+)
+
+var builder = fb.NewBuilder(0)
+var mu sync.Mutex
+
+// Get returns the current uptime as Flatbuffer serialized bytes.
+func Get() (p []byte, err error) {
+	var u up.Info
+	err = u.Get()
+	if err != nil {
+		return nil, err
+	}
+	return Serialize(&u), nil
+}
+
+// Serialize uptime.Info using Flatbuffers.
+func Serialize(u *up.Info) []byte {
+	mu.Lock()
+	defer mu.Unlock()
+	// ensure the Builder is in a usable state.
+	builder.Reset()
+	structs.InfoStart(builder)
+	structs.InfoAddTimestamp(builder, u.Timestamp)
+	structs.InfoAddUptime(builder, u.Uptime)
+	builder.Finish(structs.InfoEnd(builder))
+	p := builder.Bytes[builder.Head():]
+	// copy them (otherwise gets lost in reset)
+	tmp := make([]byte, len(p))
+	copy(tmp, p)
+	return tmp
+}
+
+// Deserialize takes some Flatbuffer serialized bytes and deserialize's them
+// as uptime.Info.
+func Deserialize(p []byte) *up.Info {
+	uF := structs.GetRootAsInfo(p, 0)
+	var u up.Info
+	u.Timestamp = uF.Timestamp()
+	u.Uptime = uF.Uptime()
+	return &u
+}
+
+// Ticker delivers loadavg.LoadAvg as Flatbuffers serialized bytes at
+// intervals.
+type Ticker struct {
+	*joe.Ticker
+	Data chan []byte
+}
+
+// NewTicker returns a new Ticker continaing a Data channel that delivers
+// the data at intervals and an error channel that delivers any errors
+// encountered.  Stop the ticker to signal the ticker to stop running; it
+// does not close the Data channel.  Close the ticker to close all ticker
+// channels.
+func NewTicker(d time.Duration) (joe.Tocker, error) {
+	t := Ticker{Ticker: joe.NewTicker(d), Data: make(chan []byte)}
+	go t.Run()
+	return &t, nil
+}
+
+// Run runs the ticker.
+func (t *Ticker) Run() {
+	// read until done signal is received
+	for {
+		select {
+		case <-t.Done:
+			return
+		case <-t.Ticker.C:
+			p, err := Get()
+			if err != nil {
+				t.Errs <- err
+				continue
+			}
+			t.Data <- p
+		}
+	}
+}
+
+// Close closes the ticker resources.
+func (t *Ticker) Close() {
+	t.Ticker.Close()
+	close(t.Data)
+}
