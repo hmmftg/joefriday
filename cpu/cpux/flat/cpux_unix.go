8dd4c93b8d716de6861ffb6f0d9e143acbda7473
Joel Scoble <joel.scoble@outlook.com>
2017-08-31 17:57:08 -0500

    add flatbuffer implementation

---
+++ cpu/cpux/flat/cpux_unix.go
@@ -0,0 +1,183 @@
+// Copyright 2016 Joel Scoble and The JoeFriday authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cpux (flat) handles Flatbuffer based processing of cpuX information,
+// where X is the integer of each CPU on the system, e.g. cpu0, cpu1, etc. On
+// linux systems this comes from /sys/devices/system/cpu. Not all paths are
+// available on all systems, e.g. /sys/devices/system/cpu/cpuX/cpufreq and its
+// children may not exist on some systems. If the system doesn't have a
+// particular file within this path, the field's value will be type's zero
+// value. Instead of returning a Go struct, it returns Flatbuffer serialized
+// bytes. A function to deserialize the Flatbuffer serialized bytes into a
+// cpuxo.CPU struct is provided.
+//
+// Note: the package name is cpux and not the final element of the import path
+// (flat).
+package cpux
+
+import (
+	"sync"
+
+	fb "github.com/google/flatbuffers/go"
+	"github.com/mohae/joefriday/cpu/cpux"
+	"github.com/mohae/joefriday/cpu/cpux/flat/structs"
+)
+
+// Profiler is used to process the cpux information as Flatbuffers serialized
+// bytes.
+type Profiler struct {
+	*cpux.Profiler
+	*fb.Builder
+}
+
+// Initializes and returns a cpux profiler.
+func NewProfiler() (p *Profiler, err error) {
+	prof, err := cpux.NewProfiler()
+	if err != nil {
+		return nil, err
+	}
+	return &Profiler{Profiler: prof, Builder: fb.NewBuilder(0)}, nil
+}
+
+// Get returns the cpux information as Flatbuffer serialized bytes.
+func (p *Profiler) Get() ([]byte, error) {
+	cpus, err := p.Profiler.Get()
+	if err != nil {
+		return nil, err
+	}
+	return p.Serialize(cpus), nil
+}
+
+var std *Profiler    // global for convenience; lazily instantiated.
+var stdMu sync.Mutex // protects access
+
+// Get returns the cpux information as Flatbuffer serialized bytes using the
+// package's global profiler.
+func Get() (p []byte, err error) {
+	stdMu.Lock()
+	defer stdMu.Unlock()
+	if std == nil {
+		std, err = NewProfiler()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return std.Get()
+}
+
+// Serialize serializes cpux.CPUs using Flatbuffers.
+func (p *Profiler) Serialize(cpus *cpux.CPUs) []byte {
+	// ensure the Builder is in a usable state.
+	p.Builder.Reset()
+	uoffs := make([]fb.UOffsetT, len(cpus.CPU))
+	for i, cpu := range cpus.CPU {
+		uoffs[i] = p.SerializeCPU(&cpu)
+	}
+	structs.CPUsStartCPUVector(p.Builder, len(uoffs))
+	for i := len(uoffs) - 1; i >= 0; i-- {
+		p.Builder.PrependUOffsetT(uoffs[i])
+	}
+	cpusV := p.Builder.EndVector(len(uoffs))
+	structs.CPUsStart(p.Builder)
+	structs.CPUsAddSockets(p.Builder, cpus.Sockets)
+	structs.CPUsAddCPU(p.Builder, cpusV)
+	p.Builder.Finish(structs.CPUsEnd(p.Builder))
+	b := p.Builder.Bytes[p.Builder.Head():]
+	// copy them (otherwise gets lost in reset)
+	tmp := make([]byte, len(b))
+	copy(tmp, b)
+	return tmp
+}
+
+// SerializeCPU serializes a CPU using flatbuffers and returns the resulting
+// UOffsetT.
+func (p *Profiler) SerializeCPU(cpu *cpux.CPU) fb.UOffsetT {
+	uoffs := make([]fb.UOffsetT, len(cpu.CacheIDs))
+	// serialize cache info in order; the flatbuffer table will have the info
+	// in order so a separate cache ID list isn't necessary for flatbuffers.
+	for i, id := range cpu.CacheIDs {
+		// If the ID doesn't exist, the 0 value will be used
+		inf := cpu.Cache[id]
+		uoffs[i] = p.SerializeCache(id, inf)
+	}
+	structs.CPUStartCacheVector(p.Builder, len(uoffs))
+	for i := len(uoffs) - 1; i >= 0; i-- {
+		p.Builder.PrependUOffsetT(uoffs[i])
+	}
+	cache := p.Builder.EndVector(len(uoffs))
+
+	structs.CPUStart(p.Builder)
+	structs.CPUAddPhysicalPackageID(p.Builder, cpu.PhysicalPackageID)
+	structs.CPUAddCoreID(p.Builder, cpu.CoreID)
+	structs.CPUAddMHzMin(p.Builder, cpu.MHzMin)
+	structs.CPUAddMHzMax(p.Builder, cpu.MHzMax)
+	structs.CPUAddCache(p.Builder, cache)
+	return structs.CPUEnd(p.Builder)
+}
+
+// SerializeCache serializes a cache entry using flatbuffers and returns the
+// resulting UOffsetT.
+func (p *Profiler) SerializeCache(id, inf string) fb.UOffsetT {
+	cID := p.Builder.CreateString(id)
+	cInf := p.Builder.CreateString(inf)
+	structs.CacheInfStart(p.Builder)
+	structs.CacheInfAddID(p.Builder, cID)
+	structs.CacheInfAddSize(p.Builder, cInf)
+	return structs.CacheInfEnd(p.Builder)
+}
+
+// Serialize cpux.CPUs using the package global profiler.
+func Serialize(cpus *cpux.CPUs) (p []byte, err error) {
+	stdMu.Lock()
+	defer stdMu.Unlock()
+	if std == nil {
+		std, err = NewProfiler()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return std.Serialize(cpus), nil
+}
+
+// Deserialize takes some Flatbuffer serialized bytes and deserializes them
+// as cpux.CPUs.
+func Deserialize(p []byte) *cpux.CPUs {
+	fcpus := structs.GetRootAsCPUs(p, 0)
+	l := fcpus.CPULength()
+	cpus := &cpux.CPUs{}
+	fCPU := &structs.CPU{}
+	fCache := &structs.CacheInf{}
+	cpu := cpux.CPU{}
+	cpus.Sockets = fcpus.Sockets()
+	for i := 0; i < l; i++ {
+		if !fcpus.CPU(fCPU, i) {
+			continue
+		}
+		cpu.PhysicalPackageID = fCPU.PhysicalPackageID()
+		cpu.CoreID = fCPU.CoreID()
+		cpu.MHzMin = fCPU.MHzMin()
+		cpu.MHzMax = fCPU.MHzMax()
+		caches := fCPU.CacheLength()
+		cpu.CacheIDs = make([]string, 0, caches)
+		cpu.Cache = make(map[string]string, caches)
+		for j := 0; j < caches; j++ {
+			if !fCPU.Cache(fCache, j) {
+				continue
+			}
+			cpu.CacheIDs = append(cpu.CacheIDs, string(fCache.ID()))
+			cpu.Cache[cpu.CacheIDs[j]] = string(fCache.Size())
+		}
+		cpus.CPU = append(cpus.CPU, cpu)
+	}
+	return cpus
+}
---
+++ cpu/cpux/flat/cpux_unix_test.go
@@ -0,0 +1,174 @@
+// Copyright 2016 Joel Scoble and The JoeFriday authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cpux
+
+import (
+	"encoding/json"
+	"testing"
+
+	fb "github.com/google/flatbuffers/go"
+	"github.com/mohae/joefriday/cpu/cpux"
+	"github.com/mohae/joefriday/cpu/testinfo"
+)
+
+func TestCPUX(t *testing.T) {
+	// set up test stuff w/o freq
+	tcpu := testinfo.NewTempSysDevicesSystemCPU()
+	err := tcpu.Create()
+	if err != nil {
+		t.Fatalf("setting up cpux testing info: %s", err)
+	}
+	prof := &Profiler{Builder: fb.NewBuilder(0), Profiler: &cpux.Profiler{SystemCPUPath: tcpu.Dir, NumCPU: int(tcpu.CoresPerPhysicalPackage * tcpu.PhysicalPackageCount)}}
+	p, err := prof.Get()
+	if err != nil {
+		t.Error(err)
+	}
+	cpus := Deserialize(p)
+
+	//compare results cpufreq
+	err = tcpu.ValidateCPUX(cpus)
+	if err != nil {
+		t.Error(err)
+	}
+	js, _ := json.MarshalIndent(cpus, "", "\t")
+	t.Log(string(js))
+
+	// cleanup for next
+	err = tcpu.Clean(false)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// set up test stuff w/o freq
+	tcpu.Freq = false
+	err = tcpu.Create()
+	if err != nil {
+		t.Error("setting up cpux testing info: %s", err)
+		goto multiSocket
+	}
+
+	p, err = prof.Get()
+	if err != nil {
+		t.Error(err)
+	}
+	cpus = Deserialize(p)
+
+	js, _ = json.MarshalIndent(cpus, "", "\t")
+	t.Log(string(js))
+	// compare results with frequency
+	err = tcpu.ValidateCPUX(cpus)
+	if err != nil {
+		t.Errorf("validate min/max: %s", err)
+	}
+
+	// cleanup for next
+	err = tcpu.Clean(false)
+	if err != nil {
+		t.Error(err)
+	}
+
+multiSocket:
+	// 2 sockets
+	tcpu.PhysicalPackageCount = 2
+	prof.NumCPU = int(tcpu.CoresPerPhysicalPackage * tcpu.PhysicalPackageCount)
+	tcpu.Freq = true
+	err = tcpu.Create()
+	if err != nil {
+		t.Error("setting up cpux testing info: %s", err)
+		goto noFreq
+	}
+
+	p, err = prof.Get()
+	if err != nil {
+		t.Error(err)
+	}
+	cpus = Deserialize(p)
+
+	//compare results cpufreq
+	err = tcpu.ValidateCPUX(cpus)
+	if err != nil {
+		t.Error(err)
+	}
+	js, _ = json.MarshalIndent(cpus, "", "\t")
+	t.Log(string(js))
+
+	// cleanup for next
+	err = tcpu.Clean(false)
+	if err != nil {
+		t.Error(err)
+	}
+
+noFreq:
+	// set up test stuff w/o freq
+	tcpu.Freq = false
+	err = tcpu.Create()
+	if err != nil {
+		t.Error("setting up cpux testing info: %s", err)
+		goto clean
+	}
+
+	p, err = prof.Get()
+	if err != nil {
+		t.Error(err)
+	}
+	cpus = Deserialize(p)
+
+	js, _ = json.MarshalIndent(cpus, "", "\t")
+	t.Log(string(js))
+	// compare results with frequency
+	err = tcpu.ValidateCPUX(cpus)
+	if err != nil {
+		t.Errorf("validate min/max: %s", err)
+	}
+
+clean:
+	// cleanup everything
+	err = tcpu.Clean(true)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func BenchmarkGet(b *testing.B) {
+	var jsn []byte
+	p, _ := NewProfiler()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		jsn, _ = p.Get()
+	}
+	_ = jsn
+}
+
+func BenchmarkSerialize(b *testing.B) {
+	var f []byte
+	p, _ := NewProfiler()
+	v, _ := p.Profiler.Get()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		f = p.Serialize(v)
+	}
+	_ = f
+}
+
+func BenchmarkDeserialize(b *testing.B) {
+	var cpus *cpux.CPUs
+	p, _ := NewProfiler()
+	cpusb, _ := p.Get()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		cpus = Deserialize(cpusb)
+	}
+	_ = cpus
+}
